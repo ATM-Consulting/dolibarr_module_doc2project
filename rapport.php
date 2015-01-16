76c63aa5e8473a0c69e0a2e51507e36c7e1f6d0f
geoffrey.girard <geoffre.girard@atm-consulting.fr>
2015-01-16 10:41:23 +0100

    cosm&étique

--- rapport.php
+++ rapport.php
@@ -318,6 +318,7 @@ function _print_statistiques_projet(&$TRapport){
 					<td><?php echo price(round($total_cout_homme,2)) ?></td>
 					<td<?php echo ($total_marge < 0) ? ' style="color:red" ' : ' style="color:green" ' ?>><?php echo price(round($total_marge,2)) ?></td>
 					<td></td>
+					<td></td>
 					<td<?php echo (round(($total_vente / ($total_achat + $total_ndf + $total_cout_homme)),2) < 1) ? ' style="color:red;font-weight: bold" ' : ' style="color:green" ' ?>><?php echo round(($total_vente / ($total_achat + $total_ndf + $total_cout_homme)),2); ?> </td>
 					<td><?php echo price(round($total_devis - $total_vente,2)) ?></td>
 				</tr>
