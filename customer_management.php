7f7ee90c379dd361e9624e71ceeaba7a2c166c0c
atm-robin <robin.jaquet@atm-consulting.fr>
2016-02-17 15:34:34 +0100

    première partie du rapport. reste regroupement des doublons de propale

--- customer_management.php
+++ customer_management.php
@@ -3,6 +3,8 @@
 require('config.php');
 dol_include_once("/doc2project/lib/report.lib.php");
 dol_include_once("/doc2project/filtres.php");
+dol_include_once("../comm/propal/class/propal.class.php");
+dol_include_once("../compta/facture/class/facture.class.php");
 
 llxHeader('',$langs->trans('Report'));
 print dol_get_fiche_head(reportPrepareHead('Doc2Project') , 'Doc2Project', $langs->trans('Doc2Project'));
@@ -36,13 +38,17 @@ function _get_filtres(){
 
 //function _fiche
 function _fiche(&$PDOdb){
-	_get_infos_propal_rapport($PDOdb);
-	print '<div>';
-	$TRapport=_get_infos_rapport($PDOdb);
+	
+	_print_rapport($PDOdb);
+	//$TRapport=_get_infos_rapport($PDOdb);
+}
+
+function _print_rapport(&$PDOdb){
+	global $db;	
 	?>
 	
 	<div class="tabBar" style="padding-bottom: 25px;">
-		<table id="statistiques_projet" class="noborder" width="100%">
+		<table id="gestion_client" class="noborder" width="100%">
 			<thead>
 				<tr style="text-align:left;" class="liste_titre nodrag nodrop">
 					<th class="liste_titre">Tiers</th>
@@ -50,7 +56,7 @@ function _fiche(&$PDOdb){
 					<?php 
 					print_liste_field_titre('Date cloture', $_SERVER["PHP_SELF"], "p.datee", "", $params, "", $sortfield, $sortorder);
 					?>
-					<th class="liste_titre">Ref facture</th>
+					<th class="liste_titre">Facture</th>
 					<th class="liste_titre">Délais</th>
 					<?php
 					$TCateg = _select_categ($PDOdb);
@@ -58,20 +64,40 @@ function _fiche(&$PDOdb){
 						print '<th class="liste_titre">'.$categ['label'].'</th>';						
 					}
 					?>
-					<td class="liste_titre">note</td>
+					<td class="liste_titre">commentaires</td>
 				</tr>
 			</thead>
 			<!--AJOUTER UN BANDEAU POUR FILTRER SUR LE PROJET, ETC.. -->
 			<tbody>
-				<td></td>
-				<td></td>
-				<td></td>
-				<td></td>
-				<td></td>
 				<?php
-				foreach ($TCateg as $categ) {
-						print '<td>'.' '.'</td>';						
+				$TInfosPropal = _get_infos_propal_rapport($PDOdb);
+				foreach ($TInfosPropal as $infoLine) {
+
+					$societe= new Societe($db);
+					$societe->fetch($infoLine['socId']);
+					
+					$propal=new Propal($db);
+					
+					$propal->fetch($infoLine['propId']);
+					
+					$facture=new Facture($db);
+					$facture->fetch($infoLine['facId']);
+					
+					
+					print '<tr>';
+					print '<td>'.$societe->getNomUrl(1,'').'</td>';
+					print '<td>'.$propal->getNomUrl(1,'').'</td>';
+					print '<td>'.$infoLine['prop_cloture'].'</td>';
+					print '<td>'.$facture->getNomUrl(1,'').'</td>';
+					print '<td>'.$infoLine[''].'</td>';
+					
+					
+					foreach ($TCateg as $categ) {
+						
 					}
+					print '<td>'.$infoLine['proj_note'].'</td>';
+					print '</tr>';											
+				}
 				?>
 				<td></td>
 			</tbody>
@@ -93,23 +119,20 @@ function _fiche(&$PDOdb){
 		</table>
 	</div>
 	<?php
-	
-	print '</div>';
 }
 
 function _get_infos_categs_rapport($PDOdb){
-	$TReport=array();
-	/*
-	 *Requete SQL 
-	*/
 	
+	$sql='SELECT ';
+	
+	$TReport=array();
 	
 	return $TReport;
 }
 
 function _get_infos_propal_rapport($PDOdb){
 	$sql = 'SELECT soc.nom AS soc_name, soc.rowid AS socId, prop.ref AS prop_ref, prop.rowid AS propId, prop.date_cloture AS prop_cloture, 
-	fact.rowid AS facId, fact.facnumber AS facnumber, fact.fk_statut AS fac_statut, proj.note_private AS proj_note 
+	fact.rowid AS facId, fact.facnumber AS facnumber, fact.fk_statut AS fac_statut, proj.note_private AS proj_note, proj.rowid AS id_project  
 	FROM '.MAIN_DB_PREFIX.'societe soc INNER JOIN '.MAIN_DB_PREFIX.'propal prop ON soc.rowid=prop.fk_soc 
 	INNER JOIN '.MAIN_DB_PREFIX.'element_element el ON el.fk_source = prop.rowid 
 	INNER JOIN '.MAIN_DB_PREFIX.'facture fact ON el.fk_target = fact.rowid 
@@ -130,7 +153,8 @@ function _get_infos_propal_rapport($PDOdb){
 						"facId"        => $PDOdb->Get_field('facId'),
 						"facnumber"    => $PDOdb->Get_field('facnumber'),
 						"fac_statut"   => $PDOdb->Get_field('fac_statut'),
-						"proj_note"    => $PDOdb->Get_field('proj_note')						
+						"proj_note"    => $PDOdb->Get_field('proj_note'),	
+						"id_project"   => $PDOdb->Get_field('id_project')					
 					);
 	}
 	//var_dump($TInfosPropal);
